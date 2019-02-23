e4a0fdddba1705803dbb88807dd5c1f85b9d8e9c
Tom Chambers <tom.chambers@gmail.com>
2019-02-23 16:22:27 +0000

    WIP adding adam optmizer

--- complete.py
+++ complete.py
@@ -8,16 +8,15 @@ import PIL
 import PIL.Image as Image
 import scipy.misc
 
-ITERATIONS = 2
-
-sess = tf.InteractiveSession()
-
-counter = 0
+ITERATIONS = 200
+LEARNING_RATE = 0.1
 
 
 def save_image(image):
     global counter
     counter += 1
+    if counter % 50 > 0:
+        return
     images = np.array([image])
     images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0,
                      255.0).astype(np.uint8)  # [-1,1] => [0,255]
@@ -26,9 +25,15 @@ def save_image(image):
         'img-sample-' + str(counter) + '.png')
 
 
+sess = tf.InteractiveSession()
+
+counter = 0
+v = 0
 z = np.random.randn(1, 512)
 tensorZ = tf.Variable(z)
 
+
+optimizer = tf.train.AdamOptimizer()
 init = tf.global_variables_initializer()
 sess.run(init)
 
@@ -37,68 +42,48 @@ with open('/results/005-pgan-customimages-preset-v2-1gpu-fp32/network-snapshot-0
 
 labels = np.zeros([z.shape[0]] + Gs.input_shapes[1][1:])
 
-v = 0
-
 y = scipy.misc.imread('/source/bayeux1.png', mode='RGB').astype(np.float)
-
-# make mask
-# mask = np.ones(y.shape)
-# mask[128:, :, :] = 0.0
-
 # RESHAPE TEST IMAGE
 # y shape (256, 256, 3)
 # generated shape is (3, 256, 256)
 reshaped_y = y.transpose(2, 0, 1)
 reshaped_y = (reshaped_y / 255) * 2.0 - 1.0
 
+# make mask
+# mask = np.ones(y.shape)
+# mask[128:, :, :] = 0.0
 
 for iteration in range(ITERATIONS):
+    print("running iteration " + str(iteration))
+
     # GENERATE IMAGE USING G() AND SAVE
     Gz_tensor = Gs.get_output_for(tensorZ, labels)
     Gz_tensor_image = Gz_tensor[0]
-    generated_image_pixels = sess.run(Gz_tensor_image)
-    save_image(generated_image_pixels)
-
-    # CALCULATE DIFFERENCE
-    # WITH NO MASK
-    # difference = tf.abs(generated_image - reshaped_y)
-    # SAVE DIFFERENCE IMAGE
-    # difference_image = tf.Session().run(difference)
-    # save_image(difference_image)
 
     # MASK RIGHT HAND SIDE OF IMAGES BEFORE CALCULATING DIFFERENCE
-    # TODO: this
     # TODO: try adding the completed image to discriminator, not just generated image
 
     # CALCULATE CONTEXTUAL LOSS (DIFFERENCE)
     # MASK
     # contextual_loss = tf.reduce_sum(
-    #     tf.contrib.layers.flatten(tf.abs(tf.mul(mask, Gz), tf.mul(mask, y))), 1)
-    # flattened_difference = tf.contrib.layers.flatten(difference)
-    # flattened_difference_out = sess.run(flattened_difference)
-    # print("Flattened difference", flattened_difference_out)
-    # contextual_loss = tf.reduce_sum(flattened_difference)
-    # contextual_loss_out = sess.run(contextual_loss)
-    # print("Contextual loss", contextual_loss_out)
+    #     tf.contrib.layers.flatten(tf.abs(tf.mul(mask, Gz_tensor_image), tf.mul(mask, y))), 1)
+    contextual_loss = tf.reduce_sum(
+        tf.contrib.layers.flatten(tf.abs(Gz_tensor_image - reshaped_y)))
 
     # PERCEPTUAL LOSS - GET D() SCORES FOR OUTPUT OF G()
-    scores, _ = D.get_output_for(Gz_tensor)
-    image_scores = scores[0] * -1
+    # scores, _ = D.get_output_for(Gz_tensor)
+    # perceptual_loss = scores[0] * -1
 
-    # CALCULATE COMPLETE LOSS
-    # complete_loss = contextual_loss_out + 0.1 * pl
-    # print("Complete loss", complete_loss)
+    # CALCULATE COMPLETE LOSS - perceptual + contextual
+    # complete_loss = contextual_loss + 0.1 * perceptual_loss
+    complete_loss = contextual_loss
 
-    # GET GRADIENT OF LOSS
-    grad_complete_loss_graph = tf.gradients(
-        image_scores, tensorZ)
-    # grad_complete_loss_graph = tf.gradients(complete_loss, z)
-    grad_complete_loss = sess.run(grad_complete_loss_graph)
-    loss_for_image = grad_complete_loss[0]
+    # test - print the complete loss
+    complete_loss_out = sess.run(complete_loss)
+    print("Complete loss", complete_loss_out)
 
     # OPTIMIZE Z BASED ON LOSS
-    optimizer = tf.train.GradientDescentOptimizer(0.01)
-    grads_vars = optimizer.compute_gradients(image_scores, var_list=tensorZ)
+    grads_vars = optimizer.compute_gradients(complete_loss, var_list=tensorZ)
     apply = optimizer.apply_gradients(grads_vars)
     optimized = sess.run(apply)
 
